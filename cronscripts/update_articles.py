726c118a14fcd05705ae9c53fcaf25e147050eb7
jl987-Jie <jl987@cornell.edu>
2016-12-24 00:35:06 -0500

    Added python script to run update PubMed and Scopus articles.

---
+++ cronscripts/update_articles.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import requests
+
+
+url = 'http://localhost:8080/reciter/retrieve/articles/'
+
+
+def send_request():
+    response = requests.get(url)
+    print(response.json())
+
+
+def main():
+    send_request()
+
+
+if __name__ == "__main__":
+    main()
